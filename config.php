fb38033f9340fb2909038fba68c9c136945b451a
Aaron Alexander <nerdfiles@gmail.com>
2011-11-02 13:08:17 -0500

    cleanup

--- config.php
+++ config.php
@@ -1,10 +1,19 @@
 <?php
 Config::set( 'db_connection', array(
-	'connection_string'=>'mysql:host=localhost;dbname=habaritest',
-	'username'=>'root',
-	'password'=>'root',
+	'connection_string'=>'pgsql:host=localhost dbname=habaritest',
+	'username'=>'admin',
+	'password'=>'',
 	'prefix'=>'__habari'
 ));
+
+/*
+Config::set( 'db_connection', array(
+	'connection_string'=>'mysql:host=localhost;dbname=habaritest',
+	'username'=>'root',
+	'password'=>'',
+	'prefix'=>'__habari'
+));
+*/
 
 // $locale = 'en-us';
 ?>
